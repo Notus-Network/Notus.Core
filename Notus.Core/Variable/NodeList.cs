203f936f7fe2728d7e9e4c2abbb25006ebef6be6
Omer GOKSOY <goksoy.omer@gmail.com>
2022-07-21 23:46:26 +0300

    updated default node list

--- Notus.Core/Variable/NodeList.cs
+++ Notus.Core/Variable/NodeList.cs
@@ -10,9 +10,50 @@ namespace Notus.Variable
             new Dictionary<Notus.Variable.Enum.NetworkLayer,
             Dictionary<Notus.Variable.Enum.NetworkType, List<Notus.Variable.Struct.IpInfo>>>()
             {
+                // layer 1
                 {
                     Notus.Variable.Enum.NetworkLayer.Layer1,
                         new Dictionary<Notus.Variable.Enum.NetworkType,List<Notus.Variable.Struct.IpInfo>>(){
+                        {
+                            Notus.Variable.Enum.NetworkType.MainNet,
+                            new List<Notus.Variable.Struct.IpInfo>()
+                            {
+                                {
+                                    new Notus.Variable.Struct.IpInfo()
+                                    {
+                                        IpAddress="89.252.134.91",
+                                        Port=5000
+                                    }
+                                },
+                                {
+                                    new Notus.Variable.Struct.IpInfo()
+                                    {
+                                        IpAddress="89.252.184.151",
+                                        Port=5000
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            Notus.Variable.Enum.NetworkType.TestNet,
+                            new List<Notus.Variable.Struct.IpInfo>()
+                            {
+                                {
+                                    new Notus.Variable.Struct.IpInfo()
+                                    {
+                                        IpAddress="89.252.134.91",
+                                        Port=5001
+                                    }
+                                },
+                                {
+                                    new Notus.Variable.Struct.IpInfo()
+                                    {
+                                        IpAddress="89.252.184.151",
+                                        Port=5001
+                                    }
+                                }
+                            }
+                        },
                         {
                             Notus.Variable.Enum.NetworkType.DevNet,
                             new List<Notus.Variable.Struct.IpInfo>()
